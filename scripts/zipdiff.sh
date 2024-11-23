beda57c1bbd7dd65fccf722701e42db3880fc4c0
Luigi Ocone <luigi.ocone@kebula.it>
2024-11-23 19:08:47 +0100

    Added a script for diff checking between zipinfo

---
+++ scripts/zipdiff.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Default values
+ORIGINAL_DEFAULT="/home/kebula/Desktop/projects/ZipHashC2PA/data/inp/original_0.xlsx"
+MODIFIED_DEFAULT="/home/kebula/Desktop/projects/ZipHashC2PA/data/tmp/modified_0.xlsx"
+
+# Accept arguments or use defaults
+original="${1:-$ORIGINAL_DEFAULT}"
+modified="${2:-$MODIFIED_DEFAULT}"
+
+# Check if files exists
+if [[ ! -f "$original" ]]; then
+    echo "File 1 does not exist ($original)."
+    exit 1
+fi
+if [[ ! -f "$modified" ]]; then
+    echo "File 2 does not exist ($modified)"
+    exit 1
+fi
+
+# Actual zipdiff
+diff <(zipinfo -v $original) <(zipinfo -v $modified)
